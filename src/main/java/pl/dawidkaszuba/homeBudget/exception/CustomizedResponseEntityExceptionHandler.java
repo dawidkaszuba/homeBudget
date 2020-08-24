5f710351a63af5922425a19198a50173658c5fff
dawidkaszuba <d.kaszuba89@gmail.com>
2020-08-24 21:51:34 +0200

    added PlannedCashFlowAmountExceededException.java

--- src/main/java/pl/dawidkaszuba/homeBudget/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/pl/dawidkaszuba/homeBudget/exception/CustomizedResponseEntityExceptionHandler.java
@@ -69,6 +69,17 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
         return  new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(PlannedCashFlowAmountExceededException.class)
+    public final ResponseEntity<Object> handlePlannedCashFlowAmountExceededException(PlannedCashFlowAmountExceededException ex, WebRequest webRequest){
+
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(),ex.getMessage(),webRequest.getDescription(false));
+
+        return  new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers, HttpStatus status,
