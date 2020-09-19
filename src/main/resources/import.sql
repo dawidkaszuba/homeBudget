f8ec1d567354e6dc9663e626d5c3bb1352d5db6d
dawidkaszuba <d.kaszuba89@gmail.com>
2020-09-20 00:18:30 +0200

    added some actual data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,10 +9,10 @@ INSERT INTO ROLE(id,name) VALUES (1, 'admin');
 INSERT INTO ROLE(id,name) VALUES (2, 'regular');
 
 
-INSERT INTO income(amount,income_date,user_id) VALUES(2500.00,'2020-08-10',1);
-INSERT INTO income(amount,income_date,user_id) VALUES(2600.00,'2020-08-10',1);
+INSERT INTO income(amount,income_date,user_id) VALUES(2500.00,'2020-09-10',1);
+INSERT INTO income(amount,income_date,user_id) VALUES(2600.00,'2020-09-10',1);
 
-Insert into income(amount,income_date, user_id) values (5000.00, '2020-08-11',1);
+Insert into income(amount,income_date, user_id) values (5000.00, '2020-09-11',1);
 
 INSERT INTO tag(name,user_id,kind) VALUES('food',1,'NEGATIVE');
 INSERT INTO tag(name,user_id,kind) VALUES('clothes',1,'NEGATIVE');
@@ -24,17 +24,17 @@ INSERT INTO tag(name,user_id,kind) VALUES('petrol',2,'NEGATIVE');
 INSERT INTO tag(name,user_id,kind) VALUES('salary2',1,'POSITIVE');
 
 
-INSERT INTO expenditure(amount,expenditure_date,user_id) VALUES(24.56,'2020-08-12',1);
-INSERT INTO expenditure(amount,expenditure_date,note,user_id) VALUES(99.99,'2020-08-10','jacket',1);
-INSERT INTO expenditure(amount,expenditure_date,note,user_id) VALUES(52.39,'2020-08-15','fruit for birthday',2);
+INSERT INTO expenditure(amount,expenditure_date,user_id) VALUES(24.56,'2020-09-12',1);
+INSERT INTO expenditure(amount,expenditure_date,note,user_id) VALUES(99.99,'2020-09-10','jacket',1);
+INSERT INTO expenditure(amount,expenditure_date,note,user_id) VALUES(52.39,'2020-09-15','fruit for birthday',2);
 
 
-INSERT into PLANNED_CASH_FLOW (NAME,CURRENT_SUM_AMOUNT,END_DATE,IS_SUM_AMOUNT_EXCEEDED,NOTE,PLANNED_AMOUNT,START_DATE,USER_ID) VALUES('Petrol',200.00,'2020-08-31',false,'note1',300.00,'2020-08-01',1);
-INSERT into PLANNED_CASH_FLOW (NAME,CURRENT_SUM_AMOUNT,END_DATE,IS_SUM_AMOUNT_EXCEEDED,NOTE,PLANNED_AMOUNT,START_DATE,USER_ID) VALUES('Food',0.00,'2020-08-31',false,'note1',1500.00,'2020-08-01',1);
-INSERT into PLANNED_CASH_FLOW (NAME,CURRENT_SUM_AMOUNT,END_DATE,IS_SUM_AMOUNT_EXCEEDED,NOTE,PLANNED_AMOUNT,START_DATE,USER_ID) VALUES('Food in work',0.00,'2020-08-31',false,'note1',200.00,'2020-08-01',1);
+INSERT into PLANNED_CASH_FLOW (NAME,CURRENT_SUM_AMOUNT,END_DATE,IS_SUM_AMOUNT_EXCEEDED,NOTE,PLANNED_AMOUNT,START_DATE,USER_ID) VALUES('Petrol',200.00,'2020-09-31',false,'note1',300.00,'2020-09-01',1);
+INSERT into PLANNED_CASH_FLOW (NAME,CURRENT_SUM_AMOUNT,END_DATE,IS_SUM_AMOUNT_EXCEEDED,NOTE,PLANNED_AMOUNT,START_DATE,USER_ID) VALUES('Food',0.00,'2020-09-31',false,'note1',1500.00,'2020-09-01',1);
+INSERT into PLANNED_CASH_FLOW (NAME,CURRENT_SUM_AMOUNT,END_DATE,IS_SUM_AMOUNT_EXCEEDED,NOTE,PLANNED_AMOUNT,START_DATE,USER_ID) VALUES('Food in work',0.00,'2020-09-31',false,'note1',200.00,'2020-09-01',1);
 
-INSERT INTO expenditure(amount,expenditure_date,user_id,PLANNED_CASH_FLOW_ID ) VALUES(90,'2020-08-12',1,1);
-INSERT INTO expenditure(amount,expenditure_date,user_id,PLANNED_CASH_FLOW_ID ) VALUES(110,'2020-08-14',1,1);
+INSERT INTO expenditure(amount,expenditure_date,user_id,PLANNED_CASH_FLOW_ID ) VALUES(90,'2020-09-12',1,1);
+INSERT INTO expenditure(amount,expenditure_date,user_id,PLANNED_CASH_FLOW_ID ) VALUES(110,'2020-09-14',1,1);
 
 
 
