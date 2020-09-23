dfdd9a10722ced02feaf46238188468023ebec0e
dawidkaszuba <d.kaszuba89@gmail.com>
2020-09-23 12:25:05 +0200

    added Savings Repository

--- src/main/java/pl/dawidkaszuba/homeBudget/repository/SavingsRepository.java
+++ src/main/java/pl/dawidkaszuba/homeBudget/repository/SavingsRepository.java
@@ -1,2 +1,13 @@
-package pl.dawidkaszuba.homeBudget.repository;public interface SavingsRepository {
+package pl.dawidkaszuba.homeBudget.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import pl.dawidkaszuba.homeBudget.entity.Savings;
+
+@Repository
+public interface SavingsRepository extends JpaRepository<Savings, Long> {
+
+    @Query("SELECT s from Savings s where max(s.lastModificationDate) and s.user = ?1")
+    Savings getSavingsByUserAndMaxModificationDate(String userid);
 }
